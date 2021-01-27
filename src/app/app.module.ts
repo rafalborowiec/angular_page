68490e0c79883e43f3c27fb1157a51064834f5da
rafalborowiec <rafal.borowiec@me.com>
2021-01-27 21:42:48 +0100

    added AppState, ReadComponent and CreateComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ReadComponent, CreateComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
