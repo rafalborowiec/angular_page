5b2a4e95b5eeec1c9694dbe86c8f2596e526b569
rafalborowiec <rafal.borowiec@me.com>
2021-01-27 15:51:32 +0100

    added reducers

---
+++ src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,17 @@
+import { Action } from "@ngrx/store";
+import { Tutorial } from "./../models/tutorial.model";
+import * as TutorialActions from "./../actions/tutorial.actions";
+
+const initialState: Tutorial = {
+  name: "First Tutorial",
+  url: "http://google.com"
+};
+
+export function reducer(state: Tutorial[], action: TutorialActions.Actions) {
+  switch (action.type) {
+    case "ADD_TUTORIAL":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
