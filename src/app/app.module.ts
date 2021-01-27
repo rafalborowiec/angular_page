1bd7df804d2bcba32f139a23e0dbbf43ca20a6fb
rafalborowiec <rafal.borowiec@me.com>
2021-01-27 21:58:10 +0100

    some Read from store + dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { StoreModule } from "@ngrx/store";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ReadComponent } from './read/read.component';
-import { CreateComponent } from './create/create.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { ReadComponent } from "./read/read.component";
+import { CreateComponent } from "./create/create.component";
+import { reducer } from "./reducers/tutorial.reducer";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ReadComponent, CreateComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, ReadComponent, CreateComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Observable } from "@rxjs/Observable";
+import { Observable } from "rxjs";
 import { Store } from "@ngrx/store";
 import { Tutorial } from "./../models/tutorial.model";
 import { AppState } from "./../app.state";
@@ -10,7 +10,10 @@ import { AppState } from "./../app.state";
   styleUrls: ["./read.component.css"]
 })
 export class ReadComponent implements OnInit {
-  constructor() {}
+  tutorials: Observable<Tutorial[]>;
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select("tutorial");
+  }
 
   ngOnInit() {}
 }
